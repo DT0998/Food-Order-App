8372934034879f36690bb0625282f003a0c6076f
DT0998 <duongkytrieuvn@gmail.com>
2022-03-20 22:17:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
+function App() {
+  return (
+    <div>
+      <Fragment>
+      <Header/>
+      <main>
+        <Meals/>
+      </main>
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,20 @@
+import classes from './MealItem.module.css'
+import MealItemsForm from './MealItemsForm';
+
+function MealItem(props) {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li>
+      <div className={classes.meal}>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+      <MealItemsForm id={props.id}/>
+      </div>
+    </li>
+  );
+}
+
+export default MealItem;
